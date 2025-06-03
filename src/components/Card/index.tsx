e310af9e3fbe61abeaf8d717428b8df5a15ad8b7
vivi ciou <viviqoo02x@gmail.com>
2025-06-04 00:19:13 +0800

    feat: improve Card component styling with hover effects and image adjustments and enhance Card component layout and styling for better presentation

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,10 +4,34 @@ type CardProps = { pokemon: PokemonDetail };
 
 const Card = ({ pokemon }: CardProps) => {
   return (
-    <div className="justify-items-center p-4 w-3xs border-2 border-gray-50 rounded-md  shadow-md hover:shadow-lg transition-shadow duration-100">
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-
-      <div>{pokemon.name}</div>
+    <div className="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl hover:drop-shadow-md">
+      <div className="md:flex">
+        <div className="md:shrink-0">
+          <img
+            className="h-48 w-full object-contain md:h-full md:w-64"
+            src={pokemon.sprites.other["official-artwork"].front_default}
+            alt={pokemon.name}
+          />
+        </div>
+        <div className="p-8 bg-zinc-50">
+          <div className="text-sm font-semibold tracking-wide text-indigo-500 uppercase">
+            {pokemon.id}
+          </div>
+          <a
+            href="#"
+            className="mt-1 block text-lg leading-tight font-medium text-black hover:underline"
+          >
+            {pokemon.name}
+          </a>
+          <div className="flex items-center mt-2">
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <img src={pokemon.sprites.back_default} alt={pokemon.name} />
+          </div>
+          <p className="mt-2 text-gray-500 ">
+            {pokemon.types.map((type) => type.type.name).join(", ")}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
