39749f4652a9e34dd1d10225775ec5ca9e592a8e
vivi ciou <viviqoo02x@gmail.com>
2025-06-14 12:24:23 +0800

    feat: add path aliasing and update TypeScript configuration for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import { alias } from "./src/utils/alias-path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), viteTsconfigPaths()],
+  resolve: {
+    alias,
+  },
 });
