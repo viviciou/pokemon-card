85d59f7ad747c0c2b53abb3d0264a801826caa28
vivi ciou <viviqoo02x@gmail.com>
2025-06-14 12:25:02 +0800

    feat: refactor Pokemon card application structure with LazyLoad component and update PokemonList for improved loading experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import Card from "./components/Card";
-import usePokemon from "./hooks/usePokemon";
+import PokemonList from "./pages/PokemonList";
 
 function App() {
-  const { pokemons } = usePokemon();
-
-  if (!pokemons) return <></>;
-
   return (
     <>
       <div className="flex justify-center text-4xl m-8">
         Pokemon Card LazyLoad
       </div>
-      <div className="flex flex-row flex-wrap justify-between m-4 gap-4">
-        {pokemons.map((pokemon, index) => (
-          <Card key={index} pokemon={pokemon} />
-        ))}
-      </div>
+      <PokemonList />
     </>
   );
 }
